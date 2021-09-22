d8de264d4758b5cd281aae9e1f0c45bedd39abd3
Madison <madivallarino@gmail.com>
2021-09-22 12:00:50 -0400

    commit tree b404ba070da9f6a1621b7a908d49fa433e050894

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
 
+    def name 
+        @name
+    end
+    def name=(n)
+        @name = n
+    end
+
+    def job
+        @job
+    end
+
+    def job=(j)
+        @job = j
+    end
 end
